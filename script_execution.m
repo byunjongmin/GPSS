dc5140444d760497e3000a2ae07be095756f6151
Jongmin Byun <cyberzen.byun@gmail.com>
2019-10-14 12:08:08 +0900

    Commit with a execution script to automate compiling c mex files in the GPSS

--- script_execution.m
+++ script_execution.m
@@ -1,4 +1,11 @@
 % script for the execution of the GPSS
 
+mex -v CollapseMex.c
+mex -v EstimateDElevByFluvialProcess.c
+mex -v EstimateDElevByFluvialProcessBySDS.c
+mex -v EstimateSubDTMex.c
+mex -v EstimateUpstreamFlow.c
+mex -v HillslopeProcessMex.c
+
 profile on
 GPSSMain_Hy('parameter_20160306_1208.txt')
